64d9e792af6b0ced0894289f952f2c4f8fd78133
leenarie <KATIDARK@MAIL.RU>
2020-05-13 15:31:12 +0500

    Добавьте файлы проекта.

---
+++ zadanie1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(lib1.stpow("Love ", 4) + "\n");
+            Console.WriteLine(lib1.rev("Love") + "\n");
+            Console.WriteLine(lib1.remov("Победа", "Поб") + "\n");
+            Console.WriteLine(lib1.leng("Koronavirus") + "\n");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ zadanie1/lib1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie1
+{
+    class lib1
+    {
+
+        static public string stpow(string a, int b)
+        {
+
+            string nstr = "";
+            for (int i = 0; i<b;i++)
+            {
+                nstr += a;
+            }
+            return nstr;
+
+        }
+
+        static public string rev(string a)
+        {
+
+            string nstr = "";
+
+            for (int i = a.Length-1; i >= 0; i--)
+            {
+                nstr += a[i];
+            }
+
+            return nstr;
+
+        }
+
+        static public string remov(string str, string s)
+        {
+
+            string nstr = str.Replace(s,"");
+
+            return nstr;
+        }
+
+        static public int leng(string str)
+        {
+
+            return str.Length;
+
+
+        }
+
+    }
+}
